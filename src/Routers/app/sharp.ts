036e52a96b331764f1efb22adf30706096d9de81
Turria101 <94504141+Turria101@users.noreply.github.com>
2022-09-21 16:34:57 +0200

    update sharp.ts

--- src/Routers/app/sharp.ts
+++ src/Routers/app/sharp.ts
@@ -1,7 +1,8 @@
+//import sharp lib
 import sharp from  "sharp"
-
+// create async function to resize the following
 async function  resize(path:string,width:number,height:number,output:string) {
-     
+// send the output after resize      
 return  sharp(path).resize(width,height).toFile(output)
 
 }
